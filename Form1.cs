876a9e113b79adcebf9f3d0058fc97a9d727ae69
Dinar Churakov <dyrelosh@gmail.com>
2021-12-02 18:59:29 +0300

    Выполнение задания 1_2

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,36 @@ namespace Praktika
         {
             InitializeComponent();
         }
+
+        private void btn_AW_BLEND_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.AnimateWindowFlags.AW_ACTIVATE |
+                WinAPIClass.AnimateWindowFlags.AW_BLEND);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnHOR_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.AnimateWindowFlags.AW_HOR_POSITIVE |
+                WinAPIClass.AnimateWindowFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnCenter_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.AnimateWindowFlags.AW_CENTER |
+                WinAPIClass.AnimateWindowFlags.AW_CENTER);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+
+        }
     }
 }
